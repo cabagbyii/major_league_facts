14b735e6cc19aeabc3de666551848e4eeda634bf
Calvin <cabagbyii@gmail.com>
2017-10-13 17:06:06 -0400

    added homepage design

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+
 class Square extends React.Component {
   render() {
     return (
@@ -11,6 +12,8 @@ class Square extends React.Component {
     );
   }
 }
+
+
 
 class Board extends React.Component {
   renderSquare(i) {
@@ -43,18 +46,44 @@ class Board extends React.Component {
   }
 }
 
-class Game extends React.Component {
+function LeagueLogo(props) {
+  var iconClass = 'league-logo-icon '+props.league;
+  console.log(iconClass);
+  return (
+    <div className="league-logo">
+      <div className="league-logo-name">
+        {props.league}
+      </div>
+      <div className={iconClass}>
+      </div>
+    </div>
+  );
+}
+
+class Content extends React.Component {
+  renderLeagueLogos(league, position){
+    return <LeagueLogo league={league} position={position}/>
+  }
   render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+    return(
+      <div className="home-page">
+        <div className="home-page-prompt">Click to learn more about each major sports league</div>
+        <div className="home-page-league-list">
+          {this.renderLeagueLogos('NBA',0)}
+          {this.renderLeagueLogos('NFL',1)}
+          {this.renderLeagueLogos('MLB',2)}
+          {this.renderLeagueLogos('NHL',3)}
+          {this.renderLeagueLogos('MLS',4)}
         </div>
       </div>
+    );
+  }
+}
+
+class Page extends React.Component {
+  render() {
+    return (
+      <Content />
     );
   }
 }
@@ -62,6 +91,6 @@ class Game extends React.Component {
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <Page />,
   document.getElementById('root')
 );
